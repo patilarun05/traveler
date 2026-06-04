0c6edfa879fed14a10aca5c529deaa5456e26b9b
patilarun05 <patilarun05@gmail.com>
2026-06-04 15:35:38 +0530

    Code-refactor

---
+++ src/test/java/Traveler/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Traveler.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
