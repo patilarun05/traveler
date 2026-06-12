39d53ce1e715e97388cea5036702a0daba5bed63
patilarun05 <patilarun05@gmail.com>
2026-06-12 16:35:15 +0530

    java version changed

---
+++ src/main/java/Traveler/demo/HomeController.java
@@ -0,0 +1,13 @@
+package Traveler.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
